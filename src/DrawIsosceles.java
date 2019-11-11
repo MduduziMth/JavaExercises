4a7d3a7fa856f57189b75714d8b4296ea9ab5490
Mduduzi <MduduziMth@gmail.com>
2019-11-11 14:12:20 +0200

    initial Commit

---
+++ src/DrawIsosceles.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DrawIsosceles {
+    public static void main(String args[])
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter Of traingles Number");
+        int number = keyboard.nextInt();
+
+        System.out.println(isosceles(number));
+
+
+    }
+
+    public static String isosceles(int number)
+    {
+        return "";
+    }
+}
---
+++ src/HelloFunction.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloFunction {
+    public static void main(String args[])
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Hi there, what's your name?");
+        String name = keyboard.next();
+
+        System.out.print(Hello(name));
+    }
+
+    public static String Hello(String name)
+    {
+        return "Hello " + name ;
+    }
+}
